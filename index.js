b70677fb7ba2b67bf00d8cd744dbc13007e37e1e
cjw <1468733552@qq.com>
2019-12-24 15:12:11 +0800

    Init eslint-config-react

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6    : true,
+        node   : true,
+        mocha  : true,
+    },
+    globals: {
+        Atomics          : 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion : 2018,
+        sourceType  : 'module',
+        ecmaFeatures: { jsx: true },
+    },
+    plugins: [
+        'import',
+        'promise',
+        'react',
+        'react-hooks',
+    ],
+    'extends': [
+        './src/base.js',
+        './src/es6.js',
+        './src/node.js',
+        './src/promise.js',
+        './src/import.js',
+        './src/react.js',
+    ].map(require.resolve),
+    rules: {},
+};
